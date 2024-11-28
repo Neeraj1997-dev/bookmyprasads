79c547d60949790954ff9267b77322a9ef262dd2
Neeraj Kumar <neerajkumar@Neerajs-MBP.bbrouter>
2024-11-28 10:42:22 +0530

    css issue fixed .

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import './Item.css'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './Item.css';
+import { Link } from 'react-router-dom';
 
-const item = (props) => {
+const Item = (props) => {
   return (
     <div className="item">
       <Link to={`/product/${props.id}`}>
-        <img src={props.image} alt="" />
+        <img className="item-image" src={props.image} alt={props.name} />
       </Link>
-      <p>{props.name}</p>
+      <p className="item-name">{props.name}</p>
       <div className="item-prices">
         <div className="item-price-new">{props.new_price} Rs</div>
         <div className="item-price-old">{props.old_price} Rs</div>
       </div>
     </div>
   );
-}
+};
 
-export default item
+export default Item;
