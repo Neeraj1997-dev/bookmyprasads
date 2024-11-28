3eabe55db1c070e71a772bdabf5816599a920134
Neeraj Kumar <neerajkumar@Neerajs-MBP.bbrouter>
2024-11-28 10:49:14 +0530

    css issue fixed .

--- src/Components/Thankyou/Thankyou.jsx
+++ src/Components/Thankyou/Thankyou.jsx
@@ -6,39 +6,37 @@ const Thankyou = () => {
   return (
     <div className="thankyou">
       <div className="container-info">
-        <h2>Delivery Information:</h2>
+        <h2>डिलीवरी जानकारी:</h2>
         <p>
-          Your sacred prasad is on its way to you! We estimate that it will
-          reach your doorstep within 10 days. Our team is carefully preparing
-          your order to ensure it arrives with the blessings of Vaishno Devi.
+          आपका पवित्र प्रसाद आपके पास आ रहा है! हमें उम्मीद है कि यह अगले 10 दिनों के भीतर आपके दरवाजे तक पहुँच जाएगा। 
+          हमारी टीम आपके ऑर्डर को सावधानीपूर्वक तैयार कर रही है ताकि यह वैष्णो देवी के आशीर्वाद के साथ पहुंचे।
         </p>
       </div>
       <div className="container-info">
-        <h2>Need Assistance?</h2>
+        <h2>क्या आपको सहायता चाहिए?</h2>
         <p>
-          If you have any questions or concerns regarding your order, feel free
-          to reach out to our dedicated customer support team.
+          यदि आपके ऑर्डर से संबंधित कोई प्रश्न या चिंता है, तो हमारी समर्पित ग्राहक सहायता टीम से संपर्क करें।
         </p>
         <p>
-          Email:
-          <a href="mailto:neeraj@aisv.in">    neeraj@aisv.in</a>
+          ईमेल:
+          <a href="mailto:neerajkumar1997.dsa@gmail.com">    neerajkumar1997.dsa@gmail.com</a>
         </p>
         <p>
-          Phone: <a href="tel:7056330417">   7056330417</a>
+          फोन: <a href="tel:7056330417">   7056330417</a>
         </p>
       </div>
       <div className="container-info">
-        <h2>Continue Your Spiritual Journey:</h2>
+        <h2>अपनी आध्यात्मिक यात्रा जारी रखें:</h2>
         <p>
-          While you wait, why not explore more sacred offerings on our website?
-          Click the button below to continue your spiritual journey:
+          जब तक आप प्रतीक्षा कर रहे हैं, क्यों न हमारी वेबसाइट पर और पवित्र प्रसाद देखें? 
+          अपनी आध्यात्मिक यात्रा जारी रखने के लिए नीचे दिए गए बटन पर क्लिक करें:
         </p>
         <Link style={{ textDecoration: "none" }} to="/Services">
-          <div className="order-now-btn-1">Go Back to Home Page</div>
+          <div className="order-now-btn-1">होम पेज पर वापस जाएं</div>
         </Link>
       </div>
     </div>
   );
 }
 
-export default Thankyou
+export default Thankyou;
