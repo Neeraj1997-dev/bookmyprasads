496b198de24c47b0692b7de83a0ec43eb556fc0f
Neeraj Kumar <neerajkumar@Neerajs-MBP.bbrouter>
2024-11-29 16:51:05 +0530

    dev changes

--- src/Components/Assets/data.js
+++ src/Components/Assets/data.js
@@ -6,28 +6,28 @@ import p4_img from "./babadham-prasadi.jpg";
 let data_product = [
   {
     id: 1,
-    name: "दिव्य आशीर्वाद प्रसाद-1",
+    name: "माँ वैष्णो देवी प्रसाद",
     image: p2_img,
     new_price: 499,
     old_price: 700,
   },
   {
     id: 2,
-    name: "दिव्य आशीर्वाद प्रसाद-2",
+    name: "बाबा धाम प्रसाद",
     image: p4_img,
     new_price: 599,
     old_price: 899,
   },
   {
     id: 3,
-    name: "दिव्य आशीर्वाद प्रसाद-3",
+    name: "उज्जैन महाकाल प्रसाद",
     image: p1_img,
     new_price: 699,
     old_price: 1249,
   },
   {
     id: 4,
-    name: "दिव्य आशीर्वाद प्रसाद-4",
+    name: "गंगोत्री गंगाजल",
     image: p3_img,
     new_price: 699,
     old_price: 1249,
--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -6,7 +6,7 @@ import Item from '../Item/Item';
 const Popular = () => {
   return (
     <div className="popular">
-      <h1>हमारे प्रसाद</h1>
+      <h1>हमारे प्रसाद + गंगाजल</h1>
       <hr />
       <div className="popular-item">
         {data_product.map((item, i) => (
